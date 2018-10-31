1f1ba15c9b97ad88a6c5319198158eaaa4ebc12b
Luciano Nooijen <lucianonooijen@gmail.com>
2018-10-31 01:24:56 +0100

    Added intial database migration

---
+++ migrations/20181030235206_initial_setup.js
@@ -0,0 +1,62 @@
+/* eslint newline-per-chained-call: ["error", { "ignoreChainWithDepth": 10 }] */
+/* eslint-disable prettier/prettier, max-len, arrow-body-style */
+
+const createAuthorsTable = table => {
+    table.increments('id');
+    table.string('name').notNullable();
+    table.string('image_url').notNullable();
+    table.string('role').notNullable();
+};
+
+const createUsersTable = table => {
+    table.increments('id');
+    table.string('username').notNullable().unique();
+    table.string('email').notNullable().unique();
+    table.string('first_name').notNullable();
+    table.string('last_name').notNullable();
+    table.string('password').notNullable();
+    table.integer('author_id').references('id').inTable('authors');
+};
+
+const createArticlesTable = (table, knex) => {
+    table.increments('id');
+    table.integer('author').notNullable();
+    table.string('title').notNullable();
+    table.string('subtitle').notNullable();
+    table.string('slug').notNullable().unique();
+    table.string('category').notNullable();
+    table.date('posted_on').notNullable().defaultTo(knex.fn.now());
+    table.boolean('hidden').defaultTo(false);
+};
+
+const createArticleContentTable = table => {
+    table.integer('article_id').notNullable().unique().references('id').inTable('articles');
+    table.string('summary').notNullable();
+    table.string('image_url').notNullable();
+    table.text('markdown_content').notNullable();
+};
+
+const createRelatedArticlesTable = table => {
+    table.integer('article_id').notNullable().unique().references('id').inTable('articles');
+    table.integer('related_article_id').notNullable().references('id').inTable('articles');
+};
+
+exports.up = (knex, Promise) => {
+    return Promise.all([
+        knex.schema.createTable('users', table => createUsersTable(table)),
+        knex.schema.createTable('authors', table => createAuthorsTable(table)),
+        knex.schema.createTable('articles', table => createArticlesTable(table, knex)),
+        knex.schema.createTable('article_content', table => createArticleContentTable(table)),
+        knex.schema.createTable('related_articles', table => createRelatedArticlesTable(table)),
+    ]);
+};
+
+exports.down = (knex, Promise) => {
+    return Promise.all([
+        knex.schema.dropTable('related_articles'),
+        knex.schema.dropTable('article_content'),
+        knex.schema.dropTable('articles'),
+        knex.schema.dropTable('users'),
+        knex.schema.dropTable('authors'),
+    ]);
+};
