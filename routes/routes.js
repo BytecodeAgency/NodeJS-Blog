ba3e4d1d6dc89b3c7f80e93c27534e7ee5494e8c
Luciano Nooijen <lucianonooijen@gmail.com>
2018-11-03 16:05:50 +0100

    Added router

--- routes/routes.js
+++ routes/routes.js
@@ -1,1 +1,10 @@
 const controllers = require('../controllers');
+const router = require('express').Router();
+
+const {
+    RootController,
+} = controllers;
+
+router.get('/', RootController);
+
+module.exports = router;
