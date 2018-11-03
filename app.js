b2e91f1fb56143dacbf38af9d209415ee735669e
Luciano Nooijen <lucianonooijen@gmail.com>
2018-11-03 16:06:02 +0100

    Configuered app

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 /* eslint-disable */
-const { NODE_ENV, PORT, ENABLE_SQREEN } = process.env;
 require('dotenv').config();
+const { NODE_ENV, PORT, ENABLE_SQREEN } = process.env;
 if (ENABLE_SQREEN === 'true' && NODE_ENV !== 'test') require('sqreen');
 /* eslint-enable */
 
-console.log(PORT);
-console.log(NODE_ENV);
+const express = require('express');
+const routes = require('./routes');
+const middleware = require('./middleware');
+const logger = require('./helpers/logger');
+
+const app = express();
+middleware.register(app);
+app.use('/', routes);
+
+app.listen(PORT, () => {
+    logger.info(`Server listening on port ${PORT}`); // eslint-disable-line
+});
