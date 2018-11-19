e8c19745f5dcbd24bc39f05139f7b041aa9c1d82
Luciano Nooijen <lucianonooijen@gmail.com>
2018-11-19 19:46:22 +0100

    Finished initial tests for knexfile generator

--- tests/knexfile-generator.test.js
+++ tests/knexfile-generator.test.js
@@ -35,13 +35,14 @@ const testNodeBlogConfig = {
     debug: true,
 };
 
-describe('generateKnexConfig', () => {
+describe('Knexfile generator', () => {
     const defaultKnexConfig = generateKnexConfig();
     const testKnexConfig = generateKnexConfig(testNodeBlogConfig);
 
-    test('Jest should run under "test" node_env', () => {
+    test('Jest should run under "test" node_env (knexfile generator)', () => {
         expect(NODE_ENV).toBe('test');
     });
+
     test('If no config is given, fall back on env variables', () => {
         expect(defaultKnexConfig.client).toBe(DB_CLIENT_TEST);
         expect(defaultKnexConfig.connection.host).toBe(DB_HOST_TEST);
@@ -51,11 +52,13 @@ describe('generateKnexConfig', () => {
         expect(defaultKnexConfig.debug).toBe(KNEX_DEBUG_BOOL);
         expect(defaultKnexConfig.asyncStackTraces).toBe(KNEX_DEBUG_BOOL);
     });
+
     test('Debug should be turned off by default', () => {
         expect(defaultKnexConfig.debug).toBe(false);
         expect(defaultKnexConfig.asyncStackTraces).toBe(false);
     });
-    test('If a config is given, it should be loaded', () => {
+
+    test('If a config is given, it should be loaded and returned', () => {
         expect(testKnexConfig.client).toBe(testNodeBlogConfig.client);
         expect(testKnexConfig.connection.host).toBe(testNodeBlogConfig.host);
         expect(testKnexConfig.connection.database).toBe(
@@ -67,5 +70,26 @@ describe('generateKnexConfig', () => {
         );
         expect(testKnexConfig.debug).toBe(testNodeBlogConfig.debug);
         expect(testKnexConfig.asyncStackTraces).toBe(testNodeBlogConfig.debug);
+    });
+
+    test('Knexfile defaults should be set based on NODE_ENV', () => {
+        const developmentDefaults = getNodeEnvSpecificDefaults('development');
+        const testDefaults = getNodeEnvSpecificDefaults('test');
+        const productionDefaults = getNodeEnvSpecificDefaults('production');
+        expect(developmentDefaults).not.toEqual(testDefaults);
+        expect(developmentDefaults).toEqual(productionDefaults);
+        expect(developmentDefaults.client).toBe(DB_CLIENT);
+        expect(developmentDefaults.host).toBe(DB_HOST);
+        expect(developmentDefaults.database).toBe(DB_NAME);
+        expect(developmentDefaults.user).toBe(DB_USER);
+        expect(developmentDefaults.password).toBe(DB_PASS);
+    });
+
+    test('Config entries should return value, otherwise default', () => {
+        const testValue = 'givenvalue';
+        const configWithValueGiven = getConfigEntry('client', testValue);
+        const configWithoutValueGiven = getConfigEntry('client');
+        expect(configWithValueGiven).toBe(testValue);
+        expect(configWithoutValueGiven).toBe(process.env.DB_CLIENT_TEST);
     });
 });
