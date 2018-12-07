6b218be1760a65e9e3e2d5765349048d9feb28f6
Luciano Nooijen <luciano@bytecode.nl>
2018-12-07 02:59:28 +0100

    Added first working version of CLI

--- cli/add-post.ts
+++ cli/add-post.ts
@@ -1,5 +1,43 @@
-function addPost() {
-    console.log('AddPost Working!');
-}
+/* tslint:disable: no-console */
+
+import chalk from 'chalk';
+import inquirer from 'inquirer';
+import articles = require('../controllers/articles');
+
+const { addArticle, getArticle } = articles;
+
+const prompt = inquirer.createPromptModule();
+
+const addPostQuestions = [
+    { type: 'input', name: 'title', message: 'What is the post title' },
+    { type: 'input', name: 'subtitle', message: 'And the post subtitle' },
+    { type: 'input', name: 'slug', message: 'And the post slug' },
+    { type: 'input', name: 'author', message: 'What is the author id' },
+    { type: 'input', name: 'category', message: 'What is the category id' },
+    { type: 'input', name: 'summary', message: 'Please give a short summary' },
+    { type: 'input', name: 'image_url', message: 'What is the image url' },
+    { type: 'input', name: 'html_content', message: 'Minified HTML string' },
+    { type: 'input', name: 'related_articles',
+        message: 'Related articles, comma seperated, no spaces, ("1,2,3")' },
+];
+
+const createPostDataObject = input => {
+    const relatedArticleString = input.related_articles;
+    const relatedArticlesArray = relatedArticleString.split(',');
+    const postData = { ...input, related_articles: relatedArticlesArray };
+    return postData;
+};
+
+const addPost = async knex => {
+    const postDataInputRaw = await prompt(addPostQuestions);
+    const postDataInput = createPostDataObject(postDataInputRaw);
+    try {
+        const addedArticle = await addArticle(knex, postDataInput);
+        console.log(`${chalk.green('Success')}: article added!`);
+    } catch (err) {
+        console.log(`${chalk.red('Error')}: the CLI failed you :(`);
+        console.log(err);
+    }
+};
 
 export default addPost;
--- cli/index.ts
+++ cli/index.ts
@@ -1,9 +1,11 @@
-/* tslint:disable: no-console */
+/* tslint:disable: no-console import-name */
 
 import chalk from 'chalk';
 import figlet from 'figlet';
 import inquirer from 'inquirer';
+import knexGenerator from 'knex';
 
+import generateKnexfile = require('../database/generate-knexfile');
 import addPost from './add-post';
 
 const prompt = inquirer.createPromptModule();
@@ -22,16 +24,40 @@ const intro = () => {
             figlet.textSync('NodeJS Blog', { horizontalLayout: 'full' })));
 };
 
-const promptHandler = async () => {
+const promptHandler = async knex => {
     const answer = await prompt([question]) as any; // Hack to avoid error...
     if (answer.action === 'Add post') {
-        addPost();
+        addPost(knex);
     }
 };
 
-const cli = async () => {
-    intro();
-    await promptHandler();
+const createKnexInstance = config => {
+    const knexfile = generateKnexfile(config);
+    const knex = knexGenerator(knexfile);
+    return knex;
 };
 
+const cli = async (
+    client = process.env.DB_CLIENT_TEST,
+    host = process.env.DB_HOST_TEST,
+    user = process.env.DB_USER_TEST,
+    database = process.env.DB_NAME_TEST,
+    password = process.env.DB_PASS_TEST,
+    debug = process.env.KNEX_DEBUG === 'true',
+) => {
+    intro();
+
+    try {
+        // TODO: Load the config using a promt
+        const config = { client, host, user, database, password, debug };
+        const knex = createKnexInstance(config);
+        await promptHandler(knex);
+    } catch (err) {
+        console.log(`${chalk.red('Error')}: the CLI failed you :(`);
+        console.log(err);
+    }
+};
+
+// Run CLI
+require('dotenv').config(); // Load .env file
 cli();
